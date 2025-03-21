12d43cc1065c2509cc591294834763479d57667a
Orka Arnest CRUZE <orka.cruze@ign.fr>
2025-03-21 17:16:58 +0100

    style: add "@" as alias to src

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import { join, resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": resolve(join(__dirname, "src")),
+        },
+    },
 });
