d205d04cc6969771644d967bd4d113847bfab03b
Orka Arnest CRUZE <orka.cruze@ign.fr>
2025-03-21 17:17:42 +0100

    feat: add basic react-dsfr site structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-    return <>Hello</>;
-}
+import { fr } from "@codegouvfr/react-dsfr";
 
-export default App;
+import AppHeader from "./components/Layout/AppHeader";
+import AppFooter from "./components/Layout/AppFooter";
+
+export default function App() {
+    return (
+        <>
+            <AppHeader />
+            <main id="main" role="main">
+                <div className={fr.cx("fr-container")}>Hello</div>
+            </main>
+            <AppFooter />
+        </>
+    );
+}
---
+++ src/components/Layout/AppFooter.tsx
@@ -0,0 +1,79 @@
+import { headerFooterDisplayItem } from "@codegouvfr/react-dsfr/Display";
+import Footer from "@codegouvfr/react-dsfr/Footer";
+import { memo } from "react";
+
+import { FooterConsentManagementItem, FooterPersonalDataPolicyItem } from "@/components/ConsentManagement";
+
+import logoIgn from "@/img/partners-logos/logo-ign.png";
+import logoMinistereTransformation from "@/img/partners-logos/logo-ministere-transformation.jpg";
+import logoMinistereEcologie from "@/img/partners-logos/logo-ministere-ecologie.jpg";
+import logoCnig from "@/img/partners-logos/logo-rf-cnig.jpg";
+
+const AppFooter = () => {
+    return (
+        <Footer
+            accessibility="partially compliant"
+            accessibilityLinkProps={{
+                href: "/accessibilite",
+            }}
+            brandTop={
+                <>
+                    République
+                    <br />
+                    Française
+                </>
+            }
+            contentDescription="
+                Cartes.gouv.fr est développé par l’Institut national de l’information géographique et forestière (IGN) et ses partenaires. Le site s’appuie sur la Géoplateforme, la nouvelle infrastructure publique, ouverte et collaborative des données géographiques.
+            "
+            bottomItems={[
+                {
+                    linkProps: {
+                        href: "/conditions-generales-d-utilisation",
+                    },
+                    text: "Conditions générales d’utilisation",
+                },
+                <FooterPersonalDataPolicyItem key="footer-personal-data-policy-item" />,
+                <FooterConsentManagementItem key="footer-consent-management-item" />,
+                // Choix du thème clair/sombre
+                headerFooterDisplayItem,
+            ]}
+            homeLinkProps={{
+                href: "/",
+                title: "Accueil - cartes.gouv.fr-guichet-collaboratif",
+            }}
+            // termsLinkProps={{
+            //     ...routes.legal_notice().link,
+            // }}
+            // websiteMapLinkProps={{
+            //     ...routes.sitemap().link,
+            // }}
+            partnersLogos={{
+                sub: [
+                    {
+                        alt: "IGN",
+                        href: "https://www.ign.fr",
+                        imgUrl: logoIgn,
+                    },
+                    {
+                        alt: "MINISTÈRE DE LA TRANSFORMATION ET DE LA FONCTION PUBLIQUES",
+                        href: "https://www.transformation.gouv.fr/",
+                        imgUrl: logoMinistereTransformation,
+                    },
+                    {
+                        alt: "MINISTÈRE DE LA TRANSITION ÉCOLOGIQUE ET DE LA COHÉSION DES TERRITOIRES",
+                        href: "https://www.ecologie.gouv.fr/",
+                        imgUrl: logoMinistereEcologie,
+                    },
+                    {
+                        alt: "Conseil National de l’Information Géolocalisée",
+                        href: "https://cnig.gouv.fr/",
+                        imgUrl: logoCnig,
+                    },
+                ],
+            }}
+        />
+    );
+};
+
+export default memo(AppFooter);
