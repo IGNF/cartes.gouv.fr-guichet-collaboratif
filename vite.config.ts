e6ea0c3690d412ad1fa0e456230c786e38c8f2a6
Mohammed OUAZZA <mohammed.ouazza@smile.lan>
2025-03-27 13:10:49 +0100

    ci: ajouter le ci/cd github pages des 3 environnements uat/sandbox/prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/cartes.gouv.fr-guichet-collaboratif/",
     resolve: {
         alias: {
             "@": resolve(join(__dirname, "src")),
