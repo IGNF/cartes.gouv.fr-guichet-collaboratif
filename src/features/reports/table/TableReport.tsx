48480234f66c544f10b10e99839ed470c43d55b7
Mohammed OUAZZA <ouazza.mohammed95@gmail.com>
2025-07-25 09:51:41 +0200

    feat: ui report table

--- src/features/reports/table/TableReport.tsx
+++ src/features/reports/table/TableReport.tsx
@@ -1,9 +1,31 @@
+import { Table } from "@codegouvfr/react-dsfr/Table";
+import { Pagination } from "@codegouvfr/react-dsfr/Pagination";
+
+import "./TableReport.scss";
+
 const TableReport = () => {
     return (
-        <div className="table-report">
-            <h3>table des signalements</h3>
-            <div>Pagination</div>
-        </div>
+        <>
+            <Table
+                bordered
+                noCaption
+                data={[["AAA", "BBB", "CCC", "DDD", "EEE"]]}
+                fixed
+                headers={["statut", "pseudo", "date de création", "commune (département)", "thème"]}
+            />
+
+            <div className="center-pagination">
+                <Pagination
+                    count={100}
+                    defaultPage={2}
+                    getPageLinkProps={(pageNumber: number) => ({
+                        href: `#page=${pageNumber}`,
+                        "aria-label": `Aller à la page ${pageNumber}`,
+                    })}
+                    showFirstLast
+                />
+            </div>
+        </>
     );
 };
 
