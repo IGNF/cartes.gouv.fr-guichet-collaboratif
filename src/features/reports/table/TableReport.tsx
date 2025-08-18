eab7c445d60f82b4f8a353d43ef68bccfffb874a
mery <me.achemlal@gmail.com>
2025-08-18 11:25:14 +0200

    feat: add delete btn + handle search bar

--- src/features/reports/table/TableReport.tsx
+++ src/features/reports/table/TableReport.tsx
@@ -98,6 +98,7 @@ const TableReport = () => {
         addAlertMessage(StatusMessage.error, "Aucun résultat ne correspond à vos filtres.");
         return null;
     }
+
     return (
         <>
             <Button onClick={() => handleDelete()}> delete </Button>
