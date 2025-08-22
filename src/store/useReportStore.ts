566598a67fd1ac56fbd4858caeba1c677f881bcd
mery <me.achemlal@gmail.com>
2025-08-22 10:23:54 +0200

    feat: make search only on 'comment' attr

--- src/store/useReportStore.ts
+++ src/store/useReportStore.ts
@@ -1,14 +1,6 @@
 import { CommunityReport, FilterState } from "@/constants/reports/types";
 import { Feature } from "ol";
 import { create } from "zustand";
-
-interface FilterState {
-    status: string;
-    theme: string;
-    author: number | null;
-    department: string;
-}
-
 interface ReportStore {
     reports: CommunityReport[];
     selectedReport: CommunityReport | null;
