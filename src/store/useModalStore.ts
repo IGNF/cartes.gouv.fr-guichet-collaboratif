df72e4a97cc6a3f0316a973d8a363f89385858f9
mery <me.achemlal@gmail.com>
2025-10-01 10:29:17 +0200

    feat: resolving comments PR#125

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -1,11 +1,11 @@
 import { createModal } from "@codegouvfr/react-dsfr/Modal";
 
 import { create } from "zustand";
-interface useModalStore {
+interface ModalState {
     replyReportModal: ReturnType<typeof createModal>;
 }
 const confirmModal = createModal({ id: "answerreport-modal", isOpenedByDefault: false });
 
-export const useModalStore = create<useModalStore>(() => ({
+export const useModalStore = create<ModalState>(() => ({
     replyReportModal: confirmModal,
 }));
