13595b1e39f6eec387ef21e994daafeaf8450015
ibrahim-sall <ibhou2002@gmail.com>
2026-02-16 14:58:46 +0100

    fix(overviewmap): was not loading on first instance due to async from baselayer

--- src/features/navigation/controls/index.ts
+++ src/features/navigation/controls/index.ts
@@ -3,11 +3,11 @@ import { Collection } from "ol";
 import { useTranslation } from "@/i18n";
 import { translateLayerSwitcherControl } from "@/constants/communities/utils";
 import DrawingControl from "./DrawingControl";
-import useGetToolsControls from "./ToolsControl";
+import useToolsControls from "./ToolsControl";
 
 const useGetMapControls = (): Collection<Control> | Control[] | undefined => {
     const drawingControl = DrawingControl();
-    const toolsControls = useGetToolsControls();
+    const toolsControls = useToolsControls();
     const { t } = useTranslation({ useGetMapControls });
     setTimeout(() => {
         translateLayerSwitcherControl(t);
